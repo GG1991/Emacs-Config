a6ec3085be631d71c34f0dd4b0d91e89aee8022b
Guido Giuntoli <gagiuntoli@gmail.com>
2019-11-15 00:13:47 +0100

    Init Repository

---
+++ init.el
@@ -0,0 +1,31 @@
+;;
+;; Emacs Configuration File
+;;
+;; Author: Guido Giuntoli
+;;
+
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'evil)                                                               
+(evil-mode t) 
+
+(setq-default c-default-style
+	      '((java-mode . "java")
+		(awk-mode . "awk")
+		(other . "linux")))
+
