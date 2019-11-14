8fc937f142dbbca5ef8c0a11e6118dcc7a5c9a7f
Guido Giuntoli <gagiuntoli@gmail.com>
2019-11-15 00:22:44 +0100

    Adding custom

--- init.el
+++ init.el
@@ -8,22 +8,17 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil ##)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+;; Creates a custom file if not exists
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+(load custom-file)
+      
+;; Use the Evil-Mode
 (require 'evil)                                                               
 (evil-mode t) 
 
+;; C Indentation
 (setq-default c-default-style
 	      '((java-mode . "java")
 		(awk-mode . "awk")
